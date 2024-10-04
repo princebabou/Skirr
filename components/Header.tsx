daa71322b04856e53fa76482cc72890f912ad337
Prince Babou <147073041+princebabou@users.noreply.github.com>
2024-10-04 09:23:45 +0200

    changed a link

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ import { Menu, X, Sun, Moon } from 'lucide-react'
 import { useTheme } from '@/context/ThemeContext'
 
 const navigation = [
-  { name: 'Home', href: '/' },
+  { name: 'Home', href: '/blog' },
   { name: 'Blog', href: '/blog' },
   { name: 'About', href: '/about' },
   { name: 'Categories', href: '/categories' },
